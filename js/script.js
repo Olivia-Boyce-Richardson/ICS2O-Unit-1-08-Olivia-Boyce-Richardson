8df71506bfd97bd183ccdce0af4d1b47ebae53a5
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-03-08 17:41:14 +0000

    linter changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Olivia B-R All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Olivia B-R
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  alert("Hello, World!")
+}
