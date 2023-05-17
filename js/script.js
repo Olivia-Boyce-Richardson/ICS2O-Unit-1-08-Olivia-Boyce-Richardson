25885386aaa8ae7badfd25f3e66e440126ca0782
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-05-17 17:27:28 +0000

    check

--- js/script.js
+++ js/script.js
@@ -3,6 +3,16 @@
 // Created by: Olivia B-R
 // Created on: Mar 2023
 // This file contains the JS functions for index.html
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-1-08-Olivia-Boyce-Richardson/sw.js", {
+    scope: "/ICS2O-Unit-1-08-Olivia-Boyce-Richardson/",
+  })
+}
 
 function myButtonClicked() {
   alert("Hello, World!")
